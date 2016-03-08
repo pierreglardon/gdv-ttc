47500b4ed6de67f4a4a04474f76fad31b6b09ee1
VIXromane <vix.romane@gmail.com>
2016-03-08 12:35:00 +0100

    Fiche descriptive série comics OK
    fiche ok, il manque le background (class=« cover2 ») + filtres avant
    listing

--- wp-content/themes/toptencomics/template-listing.php
+++ wp-content/themes/toptencomics/template-listing.php
@@ -1,11 +1,236 @@
-<?php /* Template Name: Listing */ ?>
+<?php /* Template Name: Listing - PAGE SERIE DE COMICS */ ?>
 
 <?php get_header(); ?>
 
-<div id="page" role="main">
+<div id="page" role="main" class="cover2">
+    <section class="bloc-comics"><!-- BLOC SÉRIE COMICS -->
+        <div class=" small-12 columns fil row">
+            <ul>
+                <li><a href="#">RÉSULTATS</li></a>
+                <li><a href="#"><i class="fa fa-angle-left"></i></li></a>
+                <li><a href="#">NOM DE LA SÉRIE DE COMICS</li></a>
+            </ul>
+        </div>
 
+        <article class="row fiche-serie"><!-- DESCRIPTIF DE LA SERIE -->
+            <aside class="small-12 medium-5 columns"><!-- IMG -->
+                <img src="http://placehold.it/490x490">
+            </aside>
 
+            <aside class="small-12 medium-7 columns"><!-- TEXTE -->
+                <div class="texte-serie">
+                    <div class="square bgred btn">
+                        <a href="#!">AJOUTER À MA COLLECTION<i class="fa fa-star-o"></i></a>
+                    </div>
+                    <div class="square bgblue btn">
+                        <a href="#!">AJOUTER À MA WISHLIST<i class="fa fa-heart-o"></i></a>
+                    </div>
+                    <div class="title-serie">
+                        <h2>NOM DE LA SÉRIE DE COMICS</h2>
+                        <p>date de parution de la série</p>
+                    </div>
+                    <div class="description-scroll">
+                        <p>
+                            Earth's Mightiest Heroes reunite with their biggest guns at the forefront to take on familiar enemies and new threats alike! Featuring the work of Kurt Busiek, George Perez and other. Earth's Mightiest Heroes reunite with their biggest guns at the forefront to take on familiar enemies and new threats alike! Featuring the enemies and new threats alike! Featuring the work of Kurt
+                        </p>
+                    </div>
+                </div>
+            </aside>
 
+        </article><!-- FIN DESCRIPTIF DE LA SERIE -->
+    </section><!-- FIN BLOC SÉRIE COMICS -->
+
+    <aside class="clearfix social-networks"><!-- RÉSEAUX SOCIAUX -->
+        <ul class="float-right">
+            <li>
+                <a href="#!"><i class="fa fa-facebook-official"></i></a>
+            </li>
+            <li>
+                <a href="#!"><i class="fa fa-youtube"></i></a>
+            </li>
+            <li>
+                <a href="#!"><i class="fa fa-google-plus"></i></a>
+            </li>
+            <li>
+                <a href="#!"><i class="fa fa-twitter"></i></a>
+            </li>
+        </ul>
+    </aside><!-- FIN RÉSEAUX SOCIAUX -->
+
+    <div class="row"><!-- LISTING COMICS DE LA SERIE -->
+        <div class="small-6 medium-3 columns">
+            <div class="card">
+                <div class="tools">
+                    <div class="square bgred">
+                        <a href="#!">
+                            <i class="fa fa-star-o"></i>
+                        </a>
+                    </div>
+                    <div class="square bgblue">
+                        <a href="#!">
+                            <i class="fa fa-heart-o"></i>
+                        </a>
+                    </div>
+                </div>
+                <img src="http://placehold.it/268x411" />
+                <div class="comicTitle">
+                    <h4>Nom du comic</h4>
+                    <h5>Titre du comic</h5>
+                </div>
+            </div>
+        </div>
+        <div class="small-6 medium-3 columns">
+            <div class="card">
+                <div class="tools">
+                    <div class="square bgred">
+                        <a href="#!">
+                            <i class="fa fa-star-o"></i>
+                        </a>
+                    </div>
+                    <div class="square bgblue">
+                        <a href="#!">
+                            <i class="fa fa-heart-o"></i>
+                        </a>
+                    </div>
+                </div>
+                <img src="http://placehold.it/268x411" />
+                <div class="comicTitle">
+                    <h4>Nom du comic</h4>
+                    <h5>Titre du comic</h5>
+                </div>
+            </div>
+        </div>
+        <div class="small-6 medium-3 columns">
+            <div class="card">
+                <div class="tools">
+                    <div class="square bgred">
+                        <a href="#!">
+                            <i class="fa fa-star-o"></i>
+                        </a>
+                    </div>
+                    <div class="square bgblue">
+                        <a href="#!">
+                            <i class="fa fa-heart-o"></i>
+                        </a>
+                    </div>
+                </div>
+                <img src="http://placehold.it/268x411" />
+                <div class="comicTitle">
+                    <h4>Nom du comic</h4>
+                    <h5>Titre du comic</h5>
+                </div>
+            </div>
+        </div>
+        <div class="small-6 medium-3 columns">
+            <div class="card">
+                <div class="tools">
+                    <div class="square bgred">
+                        <a href="#!">
+                            <i class="fa fa-star-o"></i>
+                        </a>
+                    </div>
+                    <div class="square bgblue">
+                        <a href="#!">
+                            <i class="fa fa-heart-o"></i>
+                        </a>
+                    </div>
+                </div>
+                <img src="http://placehold.it/268x411" />
+                <div class="comicTitle">
+                    <h4>Nom du comic</h4>
+                    <h5>Titre du comic</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row"><!-- LISTING COMICS DE LA SERIE -->
+    <div class="small-6 medium-3 columns">
+        <div class="card">
+            <div class="tools">
+                <div class="square bgred">
+                    <a href="#!">
+                        <i class="fa fa-star-o"></i>
+                    </a>
+                </div>
+                <div class="square bgblue">
+                    <a href="#!">
+                        <i class="fa fa-heart-o"></i>
+                    </a>
+                </div>
+            </div>
+            <img src="http://placehold.it/268x411" />
+            <div class="comicTitle">
+                <h4>Nom du comic</h4>
+                <h5>Titre du comic</h5>
+            </div>
+        </div>
+    </div>
+    <div class="small-6 medium-3 columns">
+        <div class="card">
+            <div class="tools">
+                <div class="square bgred">
+                    <a href="#!">
+                        <i class="fa fa-star-o"></i>
+                    </a>
+                </div>
+                <div class="square bgblue">
+                    <a href="#!">
+                        <i class="fa fa-heart-o"></i>
+                    </a>
+                </div>
+            </div>
+            <img src="http://placehold.it/268x411" />
+            <div class="comicTitle">
+                <h4>Nom du comic</h4>
+                <h5>Titre du comic</h5>
+            </div>
+        </div>
+    </div>
+    <div class="small-6 medium-3 columns">
+        <div class="card">
+            <div class="tools">
+                <div class="square bgred">
+                    <a href="#!">
+                        <i class="fa fa-star-o"></i>
+                    </a>
+                </div>
+                <div class="square bgblue">
+                    <a href="#!">
+                        <i class="fa fa-heart-o"></i>
+                    </a>
+                </div>
+            </div>
+            <img src="http://placehold.it/268x411" />
+            <div class="comicTitle">
+                <h4>Nom du comic</h4>
+                <h5>Titre du comic</h5>
+            </div>
+        </div>
+    </div>
+    <div class="small-6 medium-3 columns">
+        <div class="card">
+            <div class="tools">
+                <div class="square bgred">
+                    <a href="#!">
+                        <i class="fa fa-star-o"></i>
+                    </a>
+                </div>
+                <div class="square bgblue">
+                    <a href="#!">
+                        <i class="fa fa-heart-o"></i>
+                    </a>
+                </div>
+            </div>
+            <img src="http://placehold.it/268x411" />
+            <div class="comicTitle">
+                <h4>Nom du comic</h4>
+                <h5>Titre du comic</h5>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 
 <?php get_footer(); ?>
