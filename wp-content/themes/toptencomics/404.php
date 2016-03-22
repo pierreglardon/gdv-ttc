9c211a2ff826a06b12ef7873eb79d33ac6da8450
VIXromane <vix.romane@gmail.com>
2016-03-22 14:48:49 +0100

    404

--- wp-content/themes/toptencomics/404.php
+++ wp-content/themes/toptencomics/404.php
@@ -8,13 +8,13 @@
 
 get_header(); ?>
 
-<div class="row">
-	<div class="small-12 large-8 columns" role="main">
 
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/heDQC2xugrU" frameborder="0" allowfullscreen></iframe>
+	<div class="videoWrapper" role="main">
+
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/heDQC2xugrU?rel=0&autoplay=1" frameborder="0" end="0,05" allowfullscreen></iframe>
 
 
 	</div>
+<img class="404" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/404.jpg" alt="erreur 404" width="35" height="35"/>
 
-</div>
 <?php get_footer();
