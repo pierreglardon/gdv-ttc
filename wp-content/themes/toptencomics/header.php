904856c0ccc5785f439e64ec7bae112057ec5110
Pierre Glardon <pierreglardoncontact@gmail.com>
2016-03-28 01:45:56 +0200

    loader

--- wp-content/themes/toptencomics/header.php
+++ wp-content/themes/toptencomics/header.php
@@ -11,7 +11,7 @@
 
 ?>
 <!doctype html>
-<html class="no-js" <?php language_attributes(); ?> >
+<html class="no-js js" <?php language_attributes(); ?>>
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -25,6 +25,7 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+	<div id="preloader"></div>
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
